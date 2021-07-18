0e75fb57b8c7d8e63e2af8911663e4234c0377fc
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-18 16:48:13 +0800

    Create Profile Page web view

---
+++ lib/pages/profile_page/profile_web_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gify/constants/styles.dart';
+import 'package:gify/widgets/profile_page_widgets/profile_item_card.dart';
+import 'package:gify/widgets/top_nav_bar.dart';
+import 'package:gify/widgets/top_nav_drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProfilePageWebView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+    return SafeArea(
+      child: Scaffold(
+        drawer: TopNavDrawer(),
+        key: _scaffoldKey,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              navigationBar(screenWidth: _width, scaffoldKey: _scaffoldKey),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: _width * 0.05),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              minRadius: 80,
+                              maxRadius: 80,
+                              backgroundImage: NetworkImage(
+                                  'https://picsum.photos/200/200'),
+                            ),
+                            SizedBox(height: 20),
+                            SelectableText(
+                              'Lorem Ipsum Dolor',
+                              style: GoogleFonts.montserrat(fontSize: 16, color: Colors.black),
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                  MaterialStateProperty.all<Color>(kDarkGreen),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ))),
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Center(
+                                  child: Text(
+                                    'Add an Item',
+                                    style: GoogleFonts.sen(
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 8,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SelectableText(
+                              'Your Item Listings',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 24,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                ProfileItemCard(),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
