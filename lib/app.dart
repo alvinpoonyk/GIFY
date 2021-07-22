36878543be9835b809107974881daee99bc48ea6
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-22 16:33:07 +0800

    Change MaterialApp to GetMaterialApp to remove dependency on context

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:gify/pages/explore_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'GIFY',
       theme: ThemeData(
         primarySwatch: Colors.blue,
