10b92ccfd4169cd139b892ca067a38c7fbbe064a
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-24 02:57:10 +0800

    Use ImageFile class to encapsulate data instead

--- lib/controllers/add_item_page_controller.dart
+++ lib/controllers/add_item_page_controller.dart
@@ -1,28 +1,57 @@
-import 'dart:typed_data';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gify/constants/styles.dart';
+import 'package:gify/repositories/items_repository.dart';
+import 'package:gify/repositories/items_repository_impl.dart';
 import 'package:gify/services/get_selected_image_bytes.dart';
+import 'package:gify/widgets/center_circular_progress_indicator.dart';
 
 class AddItemPageController extends GetxController {
 
-  var image1Bytes = ImageBytes(null).obs;
-  var image2Bytes = ImageBytes(null).obs;
+  var image1 = ImageFile(null, null).obs;
+  var image2 = ImageFile(null, null).obs;
+  var isLoading = false;
 
-  setImage1Bytes() async {
-    Uint8List bytes = await getSelectedImageBytes();
-    image1Bytes.value.bytes = bytes;
-    image1Bytes.refresh();
+  final ItemsRepository _itemsRepository = ItemsRepositoryImpl();
+
+  void setLoadingStatus({required bool loadingStatus}) {
+    isLoading = loadingStatus;
   }
 
-  setImage2Bytes() async {
-    Uint8List bytes = await getSelectedImageBytes();
-    image2Bytes.value.bytes = bytes;
-    image2Bytes.refresh();
+  void setImage1() async {
+    ImageFile imageFile1 = await getSelectedImageFile();
+    image1.value = imageFile1;
+    image1.refresh();
   }
 
+  void setImage2() async {
+    ImageFile imageFile2 = await getSelectedImageFile();
+    image2.value = imageFile2;
+    image2.refresh();
+  }
+
+  Future<void> storeItemInRemoteDatabase({
+    required String name, required String ownerID, required String location, required String category,
+    required String description, required String availability}) async {
+      Map<String, dynamic> itemData = {
+        "name" : name,
+        "ownerID" : ownerID,
+        "location" : location,
+        "category" : category,
+        "description" : description,
+        "availability" : availability,
+      };
+
+      Get.showOverlay(
+          asyncFunction: () => _itemsRepository.createItem(itemData: itemData, imageFile1: image1.value, imageFile2: image2.value),
+          loadingWidget: centerCircularProgressIndicator(),
+      );
+  }
 
 }
 
-class ImageBytes {
+class ImageFile {
+  var file;
   var bytes;
-  ImageBytes(this.bytes);
+  ImageFile(this.file, this.bytes);
 }
