2181d1694b82b41428d3e65bcbb90618b920a581
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-24 02:56:01 +0800

    Add new createItem method to interface

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -1,3 +1,4 @@
+import 'package:gify/controllers/add_item_page_controller.dart';
 import 'package:gify/models/item.dart';
 
 abstract class ItemsRepository {
@@ -6,5 +7,6 @@ abstract class ItemsRepository {
   Future<List<Item>> getItemsByCategory({required String category});
   Future<List<Item>> getItemsByLocation({required String location});
   Future<List<Item>> getItemsByCategoryAndLocation({required category, required location});
+  Future<void> createItem({required Map<String, dynamic> itemData, required ImageFile imageFile1, required ImageFile imageFile2});
 
 }
