37bfc1b09cfeefb97e0e22d49235e20f8db6ebf6
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-26 04:21:45 +0800

    Bug fixes and replace throw with rethrow

--- lib/repositories/users_repository_impl.dart
+++ lib/repositories/users_repository_impl.dart
@@ -9,36 +9,43 @@ class UsersRepositoryImpl extends UsersRepository {
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final String userStorageURL = "gs://gify-33f30.appspot.com/users/";
+  final String defaultProfileImage = "https://firebasestorage.googleapis.com/v0/b/gify-33f30.appspot.com/o/assets%2Fdefault_avatar.jpeg?alt=media&token=31dc001e-0c83-4e2d-a6d6-f09bd75bab30";
 
   @override
-  Future<void> createUser({required Map<String, dynamic> userData, required ImageFile imageFile1}) async {
+  Future<void> createUser({required Map<String, dynamic> userData, required ImageFile imageFile}) async {
     try {
       DocumentReference documentReference = _db.collection('users').doc();
       final String documentID = documentReference.id;
       final String fileName = "1";
-      final bool isUploadImageSuccess = await uploadImageToRemoteDB(storageURL: userStorageURL, imageFile: imageFile1, fileName: fileName, id: documentID);
-      if (isUploadImageSuccess) {
-        final String downloadURL = await FirebaseStorage.instance.refFromURL("$userStorageURL/$documentID/$fileName").getDownloadURL();
-        userData['profileImg'] = downloadURL;
-        await documentReference.set(userData).then((value) => print("UsersRepositoryImpl: Added user to remote database"));
+      String downloadURL = defaultProfileImage;
+
+      userData['id'] = documentID;
+
+      if (imageFile.bytes != null && imageFile.file != null) {
+        await uploadImageToRemoteDB(storageURL: userStorageURL, imageFile: imageFile, fileName: fileName, id: documentID);
+        downloadURL = await FirebaseStorage.instance.refFromURL("$userStorageURL/$documentID/$fileName").getDownloadURL();
       }
+      userData['profileImage'] = downloadURL;
+      await documentReference.set(userData).then((value) => print("UsersRepositoryImpl(createUser): Added user to remote database"));
     } catch (e) {
-      print("UsersRepositoryImpl: Fail to add user to remote database");
-      throw e;
+      print("UsersRepositoryImpl(createUser): Failed to add user to remote database");
+      rethrow;
     }
   }
 
   @override
-  Future<User> getUser({required String id}) async {
+  Future<User> getUser({required String email}) async {
     late User user;
     try {
-      await _db.collection("users").doc(id).get().then((documentSnapshot) {
-        user = User.fromJson(documentSnapshot.data()!);
+      await _db.collection("users").where("email", isEqualTo: email).limit(1).get().then((querySnapshot) {
+        Map<String, dynamic> userData = querySnapshot.docs.first.data();
+        User fetchedUser = User.fromJson(userData);
+        user = fetchedUser;
       });
       return user;
     } catch(e) {
       print("UsersRepositoryImpl: Fail to get user from remote database");
-      throw e;
+      rethrow;
     }
   }
   
