3ca52c00a76a9abd2a84700d7ff1e45b1491b0c2
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-26 04:22:45 +0800

    Update main to load the auth controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:gify/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gify/controllers/auth_controller.dart';
 
 void main() async {
   await Firebase.initializeApp();
+  Get.put(AuthController());
   runApp(MyApp());
 }
