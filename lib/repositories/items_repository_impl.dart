85cc809874cafd4a4a472b8d7f6abcccf2b966d5
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-26 18:00:01 +0800

    Added try catch to catch any errors and handle it at the controller

--- lib/repositories/items_repository_impl.dart
+++ lib/repositories/items_repository_impl.dart
@@ -11,51 +11,77 @@ class ItemsRepositoryImpl implements ItemsRepository {
 
   @override
   Future<List<Item>> getItems() async {
-    List<Item> items = [];
-    await _db.collection('items').where('isRemoved', isEqualTo: false).get().
-    then((querySnapshot) =>
-        querySnapshot.docs.forEach((documentSnapshot) {
-          items.add(Item.fromJson(documentSnapshot.data()));
-        }));
-    return items;
+    try {
+      List<Item> items = [];
+      await _db.collection('items').where('isRemoved', isEqualTo: false).get().
+      then((querySnapshot) =>
+          querySnapshot.docs.forEach((documentSnapshot) {
+            items.add(Item.fromJson(documentSnapshot.data()));
+          }));
+      print("ItemsRepositoryImpl(getItems): Successfully fetched items");
+      return items;
+    } catch(e) {
+      print("ItemsRepositoryImpl(getItems): ${e.toString()}");
+      rethrow;
+    }
   }
 
   @override
   Future<List<Item>> getItemsByCategory({required String category}) async {
-    List<Item> data = [];
-    await _db.collection('items').where('isRemoved', isEqualTo: false).where(
-        'category', isEqualTo: category).get().
-    then((querySnapshot) =>
-        querySnapshot.docs.forEach((documentSnapshot) {
-          data.add(Item.fromJson(documentSnapshot.data()));
-        }));
-    return data;
+    try {
+      List<Item> data = [];
+      await _db.collection('items').where('isRemoved', isEqualTo: false).where(
+          'category', isEqualTo: category).get().
+      then((querySnapshot) =>
+          querySnapshot.docs.forEach((documentSnapshot) {
+            data.add(Item.fromJson(documentSnapshot.data()));
+          }));
+      print("ItemsRepositoryImpl(getItemsByCategory): Successfully get items by category");
+      return data;
+    } catch(e) {
+      print("ItemsRepositoryImpl(getItemsByCategory): ${e.toString()}");
+      rethrow;
+    }
   }
 
   @override
   Future<List<Item>> getItemsByLocation({required location}) async {
-    List<Item> data = [];
-    await _db.collection('items').where('isRemoved', isEqualTo: false).where(
-        'location', isEqualTo: location).get().
-    then((querySnapshot) =>
-        querySnapshot.docs.forEach((documentSnapshot) {
-          data.add(Item.fromJson(documentSnapshot.data()));
-        }));
-    return data;
+    try {
+      List<Item> data = [];
+      await _db.collection('items').where('isRemoved', isEqualTo: false).where(
+          'location', isEqualTo: location).get().
+      then((querySnapshot) =>
+          querySnapshot.docs.forEach((documentSnapshot) {
+            data.add(Item.fromJson(documentSnapshot.data()));
+          }));
+      print("ItemsRepositoryImpl(getItemsByLocation): Successfully get items by location");
+      return data;
+    } catch(e) {
+      print("ItemsRepositoryImpl(getItemsByLocation): ${e.toString()}");
+      rethrow;
+    }
+
   }
 
   @override
   Future<List<Item>> getItemsByCategoryAndLocation(
       {required category, required location}) async {
-    List<Item> data = [];
-    await _db.collection('items').where('isRemoved', isEqualTo: false)
-        .where('category', isEqualTo: category).where(
-        'location', isEqualTo: location).get().
-    then((querySnapshot) =>
-        querySnapshot.docs.forEach((documentSnapshot) {
-          data.add(Item.fromJson(documentSnapshot.data()));
-        }));
-    return data;
+    try {
+      List<Item> data = [];
+      await _db.collection('items').where('isRemoved', isEqualTo: false)
+          .where('category', isEqualTo: category).where(
+          'location', isEqualTo: location).get().
+      then((querySnapshot) =>
+          querySnapshot.docs.forEach((documentSnapshot) {
+            data.add(Item.fromJson(documentSnapshot.data()));
+          }));
+      print("ItemsRepositoryImpl(getItemsByCategoryAndLocation): Successfully get items by category and location");
+      return data;
+    } catch(e) {
+      print("ItemsRepositoryImpl(getItemsByCategoryAndLocation): ${e.toString()}");
+      rethrow;
+    }
+
   }
 
   @override
@@ -72,11 +98,27 @@ class ItemsRepositoryImpl implements ItemsRepository {
         final String downloadURL2 = await FirebaseStorage.instance.refFromURL("$itemStorageURL/$documentID/$fileName2").getDownloadURL();
         itemData['images'] = [downloadURL1, downloadURL2];
         itemData['id'] = documentID;
-        await documentReference.set(itemData).then((value) => print("ItemsRepositoryImpl: Added item to remote database"));
+        await documentReference.set(itemData).then((value) => print("ItemsRepositoryImpl(createItem): Added item to remote database"));
       }
     } catch (e) {
-      print("ItemsRepositoryImpl: Fail to add item to remote database");
-      throw e;
+      print("ItemsRepositoryImpl(createItem): Fail to add item to remote database");
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Item>> getUserItems({required String userID}) async {
+    try {
+      List<Item> data = [];
+      await _db.collection('items').where('ownerID', isEqualTo: userID).where('isRemoved').get().then(
+              (querySnapshot) => querySnapshot.docs.forEach((documentSnapshot) {
+            data.add(Item.fromJson(documentSnapshot.data()));
+          }));
+      print("ItemsRepositoryImpl(getUserItems): Successfully fetched user items");
+      return data;
+    } catch(e) {
+      print("ItemsRepositoryImpl(getUserItems): ${e.toString()}");
+      rethrow;
     }
   }
 
