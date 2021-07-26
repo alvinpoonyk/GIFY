8369e4a1afa76328e0d98ae4a14c22fcc1aadefa
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-26 17:57:41 +0800

    Made brand logo a widget instead of a method that returns a widget

--- lib/widgets/brand_logo.dart
+++ lib/widgets/brand_logo.dart
@@ -2,6 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:gify/constants/styles.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+class BrandLogo extends StatelessWidget {
+  final double fontSize;
+  final double iconSize;
+
+  const BrandLogo({Key? key, required this.fontSize, required this.iconSize}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.spa, size: iconSize, color: kLightGreen),
+        const SizedBox(width: 10),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'GIFY',
+            style: GoogleFonts.sen(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
 Widget brandLogo({required double fontSize, required double iconSize}) => Row(
       children: [
         Icon(Icons.spa, size: iconSize, color: kLightGreen),
