45d5b782bb257993787e5525e21b98ba11d34a2b
alvinpoonyk <alvin_6991@hotmail.com>
2021-07-29 23:57:33 +0800

    Implement logic for conversation tablet and mobile page view

--- lib/pages/conversations_page/conversations_tablet_mobile_page.dart
+++ lib/pages/conversations_page/conversations_tablet_mobile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:gify/constants/styles.dart';
+import 'package:gify/controllers/conversations_page_controller.dart';
+import 'package:gify/models/conversation.dart';
 import 'package:gify/widgets/conversations_page_widgets/conversation_list_tile.dart';
 import 'package:gify/widgets/top_nav_bar.dart';
 import 'package:gify/widgets/top_nav_drawer.dart';
@@ -7,8 +11,12 @@ import 'package:google_fonts/google_fonts.dart';
 class ConversationsPageTabletAndMobileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     double _width = MediaQuery.of(context).size.width;
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+    final ScrollController _scrollController = ScrollController();
+    final ConversationsPageController _controller = Get.put(ConversationsPageController());
+
     return SafeArea(
       child: Scaffold(
         key: _scaffoldKey,
@@ -30,19 +38,41 @@ class ConversationsPageTabletAndMobileView extends StatelessWidget {
                   const SizedBox(height: 10),
                 ],
               ),
-              ListView(
-                shrinkWrap: true,
-                children: [
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                  ConversationsListTile(width: _width),
-                ],
-              )
+              StreamBuilder<List<Conversation>>(
+                  stream: _controller.getConversationsStream(userID: 'AxwkwQs6YLSrh9RYSj0J'),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<Conversation>? conversations = snapshot.data;
+                      if (conversations!.isEmpty || conversations == null)
+                        return Text('You have no conversations');
+                      return ListView.builder(
+                          addAutomaticKeepAlives: true,
+                          itemCount: conversations.length,
+                          controller: _scrollController,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) => ConversationsListTile(conversation: conversations[index]));
+                    }
+                    if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    }
+                    return Center(
+                      child: const CircularProgressIndicator(color: kLightGreen),
+                    );
+                  },
+              ),
+              // ListView(
+              //   shrinkWrap: true,
+              //   children: [
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //     ConversationsListTile(width: _width),
+              //   ],
+              // )
             ],
           ),
         ),
