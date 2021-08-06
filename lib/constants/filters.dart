916c0378cfddd812f7ac4534b927bbc04f105fc2
alvinpoonyk <alvin_6991@hotmail.com>
2021-08-06 14:53:25 +0800

    Add more locations to location filter

--- lib/constants/filters.dart
+++ lib/constants/filters.dart
@@ -13,9 +13,14 @@ const List<String> kCategories = [
 
 const List<String> kLocations = [
   kDefaultLocationFilter,
-  'Ang Mo Kio',
-  'Bishan',
-  'Khatib',
-  'Yishun',
-  'Sembawang',
+  'Admiralty', 'Aljunied', 'Ang Mo Kio', 'Bartley', 'Bayfront', 'Beauty World', 'Bedok','Bedok North',
+  'Bedok Reservoir', 'Bencoolen', 'Bendemeer', 'Bishan', 'Boon Keng', 'Boon Lay', 'Botanic Gardens', 'Braddell',
+  'Bras Basah', 'Buangkok', 'Bugis','Bukit Batok', 'Bukit Gombak', 'Bukit Panjang', 'Buona Vista', 'Caldecott', 'Cashew','Changi Airport',
+  'Chinatown', 'Chinese Garden', 'Choa Chu Kang', 'City Hall', 'Clarke Quay', 'Clementi', 'Commonwealth', 'Dakota','Dhoby Ghaut', 'Dover',
+  'Downtown', 'Esplanade', 'Eunos', 'Expo', 'Farrer Park', 'Farrer Road', 'Fort Canning', 'Geylang Bahru', 'HarbourFront', 'Haw Par Villa', 'Hillview',
+  'Holland Village', 'Hougang', 'Jalan Besar', 'Joo Koon','Jurong East','Kaki Bukit', 'Kallang', 'Kembangan', 'Kent Ridge', 'Khatib','King Albert Park', 'Kovan', 'Kranji',
+  'Labrador Park', 'Lakeside', 'Lavender','Little India', 'MacPherson','Marina Bay', 'Marsling', 'Marymount', 'Mattar', 'Mountbatten', 'Newton','Nicoll Highway', 'Novena', 'Orchard',
+  'one-north', 'Outram Park', 'Pasir Panjang', 'Pasir Ris','Paya Lebar', 'Pioneer', 'Potong Pasir', 'Promenade', 'Punggol', 'Queenstown','Raffles Place', 'Redhill',
+  'Rochor', 'Sembawang', 'Sengkang', 'Serangoon', 'Simei','Sixth Avenue', 'Somerset', 'Stadium', 'Stevens','Tai Seng', 'Tampines', 'Tampines East', 'Tampines West', 'Tan Kah Kee',
+  'Tanah Merah', 'Tanjong Pagar','Telok Ayer', 'Telok Blangah', 'Tiong Bahru','Toa Payoh', 'Ubi', 'Upper Changi', 'Woodlands', 'Woodleigh', 'Yew Tee','Yio Chu Kang', 'Yishun',
 ];
